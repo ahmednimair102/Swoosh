714f9bac10ef4839b9bbc41e8fb08a62502ef686
Ahmed Nimair <70059102@student.uol.edu.pk>
2021-08-11 19:23:13 +0500

    lesson 5

--- app/src/main/java/com/ahmed/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/ahmed/swoosh/LeagueActivity.kt
@@ -1,11 +1,24 @@
 package com.ahmed.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+       leagueNextButton.setOnClickListener {
+            val skillIntent = Intent(this,SkillActivity::class.java)
+            startActivity(skillIntent)
+        }
+
+       /* fun leagueNextClicked(view: View){
+            val skillActivity = Intent(this, SkillActivity ::class.java)
+            startActivity(skillActivity)
+        }*/
     }
 }
