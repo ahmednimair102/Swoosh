824803ed14f1b197fcdd9247d4318e9b2e7fc5eb
Ahmed Nimair <70059102@student.uol.edu.pk>
2021-08-15 01:44:42 +0500

    lesson 8 app complete

--- app/src/main/java/com/ahmed/swoosh/controller/FinishActivity.kt
+++ app/src/main/java/com/ahmed/swoosh/controller/FinishActivity.kt
@@ -7,7 +7,7 @@ import com.ahmed.swoosh.R
 import com.ahmed.swoosh.model.Player
 import kotlinx.android.synthetic.main.activity_finish.*
 
-class FinishActivity : AppCompatActivity() {
+class FinishActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
